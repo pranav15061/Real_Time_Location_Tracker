82734be4befaee13ff0d4ae76da4e4e255c38c15
Pranav <pranavbagal4@gmail.com>
2024-07-17 00:27:57 +0530

    Corrections Done

--- app.js
+++ app.js
@@ -15,14 +15,21 @@ app.set("view engine", "ejs");
 // Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, "public")));
 
+const users = {}; // To store users and their locations
+
 io.on("connection", (socket) => {
   console.log("User connected");
 
+  // Send the list of all users to the newly connected user
+  socket.emit("all-users", users);
+
   socket.on("send-location", (data) => {
+    users[socket.id] = data;
     io.emit("receive-location", { id: socket.id, ...data });
   });
 
   socket.on("disconnect", () => {
+    delete users[socket.id];
     io.emit("user-disconnected", socket.id);
     console.log("User disconnected");
   });
--- public/js/script.js
+++ public/js/script.js
@@ -1,14 +1,17 @@
 const socket = io();
 
 if (navigator.geolocation) {
-  navigator.geolocation.watchPosition(
+  navigator.geolocation.getCurrentPosition(
     (position) => {
       const { latitude, longitude } = position.coords;
-
+      console.log(`Latitude: ${latitude}, Longitude: ${longitude}`); // Debugging
       socket.emit("send-location", { latitude, longitude });
     },
     (error) => {
       console.error(error);
+      alert(
+        "Unable to retrieve your location. Please check your location settings."
+      );
     },
     {
       enableHighAccuracy: true,
@@ -18,7 +21,7 @@ if (navigator.geolocation) {
   );
 }
 
-const map = L.map("map").setView([0, 0], 16);
+const map = L.map("map").setView([0, 0], 15); // Start with a more general view
 
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   attribution: "Pranav",
@@ -26,28 +29,31 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
 
 const markers = {};
 
-
-socket.on("receive-location",(data)=>{
-    const {id,latitude,longitude} = data;
-    map.setView([latitude,longitude]);
-
-    if(markers[id])
-    {
-        markers[id].setLatLng([latitude,longitude])
+// Handle receiving all users
+socket.on("all-users", (users) => {
+  for (const id in users) {
+    const { latitude, longitude } = users[id];
+    if (!markers[id]) {
+      markers[id] = L.marker([latitude, longitude]).addTo(map);
     }
-    else
-    {
-        markers[id]=L.marker([latitude,longitude]).addTo(map);
-    }
+  }
+});
 
-})
+// Handle receiving location updates
+socket.on("receive-location", (data) => {
+  const { id, latitude, longitude } = data;
+  if (markers[id]) {
+    markers[id].setLatLng([latitude, longitude]);
+  } else {
+    markers[id] = L.marker([latitude, longitude]).addTo(map);
+  }
+  map.setView([latitude, longitude]); // Optionally focus map on latest location
+});
 
-
-socket.on("user-disconnected",(id)=>{
-
-    if(markers[id])
-    {
-        map.removeLayer(markers[id]);
-        delete markers[id];
-    }
-})
+// Handle user disconnection
+socket.on("user-disconnected", (id) => {
+  if (markers[id]) {
+    map.removeLayer(markers[id]);
+    delete markers[id];
+  }
+});
